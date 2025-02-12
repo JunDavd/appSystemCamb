b15f29d38f29cbb14d94ad1683949a23ba5ddca4
Juan Ruiz <jdruiz008@gmail.com>
2025-02-12 16:17:56 -0500

    add: modified files

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+const Dashboard = () => {
+    return <div>Dashboard</div>
+}
+
+export default Dashboard
+
+/*
+ *<button
+                    className={
+                        'px-4 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
+                    }
+                    onClick={() => {}}
+                >
+                    <Menu className={'w-3 h-4'} />
+                </button>
+ *
+ *
+ * */
